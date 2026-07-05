6687dbf00320915860ac8aa1f61a3f3079cb5b28
Kartikeya Chauhan <kc1.408596@gmail.com>
2026-07-05 16:49:12 +0530

    Initial Commit

---
+++ Healing Watch App/HealingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealingApp.swift
+//  Healing Watch App
+//
+//  Created by Kartikeya Chauhan on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Healing_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Healing/HealingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealingApp.swift
+//  Healing
+//
+//  Created by Kartikeya Chauhan on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct HealingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
